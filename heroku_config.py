ed9379070debe10f0ccdd134c9512809449a8227
FLAMING-AI <108722900+FLAMING-AI@users.noreply.github.com>
2022-07-11 17:29:58 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -4,7 +4,7 @@ import os
 class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
-    API_HASH = os.environ.get("API_HASH", None)
+    API_HASH = os.environ.get("API_HASH", "3b5e73f63810334a1d730087b552519b")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./userbot/DOWNLOADS/")
